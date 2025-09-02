c8d120ac89d9c4969cc20e73f76217e154723e9b
nakoueno <151621435+nakoueno@users.noreply.github.com>
2025-09-02 21:02:13 +0900

    Add files via upload

--- JS/style.js
+++ JS/style.js
@@ -77,16 +77,45 @@ AOS.init();
 // }
 
 
-    $(function () {
-    $('.work').click(function () {
-    // $("body").addClass("no_scroll"); // 背景固定させるクラス付与
-    var id = $(this).data('id'); // 何番目のキャプション（モーダルウィンドウ）か認識
-    $('#overlay, .modal[data-id="modal' + id + '"]').fadeIn();
-    });
-     // オーバーレイクリックでもモーダルを閉じるように
-    $('.close-button , #overlay').click(function () {
-    $("body").removeClass("no_scroll"); // 背景固定させるクラス削除
-    $('#overlay, .modal').fadeOut();
-    });
+//     $(function () {
+//     $('.work').click(function () {
+//     // $("body").addClass("no_scroll"); // 背景固定させるクラス付与
+//     var id = $(this).data('id'); // 何番目のキャプション（モーダルウィンドウ）か認識
+//     $('.overlay, .modal[data-id="modal' + id + '"]').fadeIn();
+//     });
+//      // オーバーレイクリックでもモーダルを閉じるように
+//     $('.close-button , .overlay').click(function () {
+//     $("body").removeClass("no_scroll"); // 背景固定させるクラス削除
+//     $('.overlay, .modal').fadeOut();
+//     });
+// });
+
+
+// $(function () {
+//   $('.work').click(function () {
+//     var id = $(this).data('id');
+//     $('.overlay, .modal[data-id="modal' + id + '"]').fadeIn();
+//   });
+
+//   // オーバーレイクリックでもモーダルを閉じる
+//   $('.close-button, .overlay').click(function () {
+//     $("body").removeClass("no_scroll");
+//     $('.overlay, .modal').fadeOut();
+//   });
+// });
+
+
+
+let scrollPos = 0;
+
+$('.work').click(function () {
+  scrollPos = $(window).scrollTop(); // 現在のスクロール位置を記録
+  $('body').addClass('fixed').css({ top: -scrollPos });
+  $('.overlay, .modal[data-id="modal' + $(this).data('id') + '"]').fadeIn();
 });
 
+$('.close-button, .overlay').click(function () {
+  $('body').removeClass('fixed').css({ top: '' });
+  $(window).scrollTop(scrollPos); // 元の位置に戻す
+  $('.overlay, .modal').fadeOut();
+});
